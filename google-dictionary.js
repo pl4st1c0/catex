246ee1df28ecb7a12ead896d5002dab027b353c6
rafaela <rafaela@Rafaelas-iMac.local>
2021-04-08 00:56:49 -0300

    google-dictionary

---
+++ google-dictionary.js
@@ -0,0 +1,13 @@
+const googleDictionaryApi = require("google-dictionary-api")
+const randomWords = require('random-words');
+
+googleDictionaryApi.search(randomWords()).then((results) => {
+    (async () => { 
+        //no entiendo porqué siempre me tira undefined phonetic :(
+        //hice validaciones también y blah
+        // console.log(results[0].phonetic);
+        console.log(results[0].meaning.noun[0].definition)
+    
+    })();
+    
+});
